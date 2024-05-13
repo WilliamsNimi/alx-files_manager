7ccbfd8494bba8e923e1fc0f006c068ba10002eb
Nimi Williams <williamson.nimi@gmail.com>
2024-05-13 22:19:02 +0100

    Fixed a bunch of syntax errors

--- utils/redis.js
+++ utils/redis.js
@@ -1,34 +1,34 @@
 import { createClient } from 'redis';
 
-class RedisClient{
+class RedisClient {
     constructor(){
 	const client = createClient();
 	client.on('error', err => console.log(err));
-	await client.connect();
+	client.connect();
     }
 
-    function isAlive(){
+    isAlive(){
 	if (client.isOpen){
 	    return true;
 	}
 	return false;
     }
 
-    async function get(key){
+    async get(key){
 	const value = await client.get(key);
 	return value;
     }
 
-    async function set(key, value, time){
+    async set(key, value, time){
 	await client.set(key, value, timeout=time);
     }
 
-    async function del(key){
+    async del(key){
 	await client.del(key, function(err, response){
 	    if (err) throw err;
 	    console.log(response);
 	});
     }
 }
-const redisClient = RedisClient();
+const redisClient = new RedisClient();
 module.exports = redisClient;
